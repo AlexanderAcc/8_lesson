69041c4a5039e2cb5484ec97ffd854ec6ae142ee
AlexanderAcc <124236169+AlexanderAcc@users.noreply.github.com>
2023-02-02 14:01:52 +0700

    Add files via upload

---
+++ constructor_8_less/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor_8_less
+{
+    internal class Class1
+    {
+        private string second_string;
+        private int first_int;
+
+        public Class1(string s)
+        {
+            second_string = s;
+            System.Windows.MessageBox.Show(Convert.ToString(first_int) + " " + second_string);
+        }
+        public Class1(string s, int a)
+        {
+            second_string = s;
+            first_int = a;
+            System.Windows.MessageBox.Show(Convert.ToString(first_int) + " " + second_string);
+        }
+    }
+}
---
+++ constructor_8_less/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace constructor_8_less
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(textbox1.Text == "")
+            {
+                Class1 c1 = new Class1(textbox.Text);
+            }
+            else
+            {
+                Class1 c1 = new Class1(textbox.Text, Convert.ToInt32(textbox1.Text));
+            }
+        }
+    }
+}
